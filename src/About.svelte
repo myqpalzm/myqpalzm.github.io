f526deaa0c7bbaa08e6c9344e2bc95aaefec13bd
jonathan <jonathan.franzeli@student.umn.ac.id>
2021-12-21 23:24:22 +0700

    about page responsive fix

--- src/About.svelte
+++ src/About.svelte
@@ -6,7 +6,8 @@
     <div class="container">
         <p>Hi! I'm Jonathan Franzeli, a front-end developer with a passion for learning new things! I'm currently an undergraduate student looking for opportunities to skill up. I specialize in front-end development, graphic design, and video game development, with an interest in music producing and sound engineering.</p>
         <p>This portfolio website is made with <a href="https://svelte.dev/">Svelte</a>, a Javascript framework that I'm currently learning.</p>
-        <p>Feel free to take a look at my <a href="#">resume</a>.</p>
+        <p>Feel free to take a look at my <a href="/">resume</a>.</p>
+        <p>Thank you for reading and have a nice day!</p>
     </div>
 </main>
 
@@ -23,7 +24,7 @@
 
     @media (min-width: 0px) and (max-width: 640px) {
         .container {
-            margin: 0 5em;
+            margin: 0 0em;
         }
     }
     
