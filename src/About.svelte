c99d9c8b3f31d2d769345392304a5849ea7cf242
jonathan <jonathan.franzeli@student.umn.ac.id>
2021-12-22 13:15:01 +0700

    add resume, latest build

--- src/About.svelte
+++ src/About.svelte
@@ -6,7 +6,7 @@
     <div class="container">
         <p>Hi! I'm Jonathan Franzeli, a front-end developer with a passion for learning new things! I'm currently an undergraduate student looking for opportunities to skill up. I specialize in front-end development, graphic design, and video game development, with an interest in music producing and sound engineering.</p>
         <p>This portfolio website is made with <a href="https://svelte.dev/">Svelte</a>, a Javascript framework that I'm currently learning.</p>
-        <p>Feel free to take a look at my <a href="/">resume</a>.</p>
+        <p>Feel free to take a look at my <a href="./JonathanFranzeli_Resume.pdf" target="_blank">resume</a>.</p>
         <p>Thank you for reading and have a nice day!</p>
     </div>
 </main>
@@ -14,6 +14,10 @@
 <style>
     ::-webkit-scrollbar {
         overflow: scroll;
+    }
+
+    p {
+        text-align: justify;
     }
 
     @media (min-width: 641px) {
